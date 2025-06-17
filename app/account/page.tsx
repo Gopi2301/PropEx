a81ebc242cd79c86e7d6e7c7f970228d40a885f9
Gopinath S <gopinaths2301@gmail.com>
2025-06-17 22:36:13 +0530

    user_roles, role_permissions schema added

--- app/account/page.tsx
+++ app/account/page.tsx
@@ -1,7 +1,13 @@
 const Account = () => {
     return (
-        <div>
-            <h1>Account</h1>
+        <div className="flex min-h-screen">
+           {/* left bar */}
+           <div className="w-1/4">
+             
+           </div>
+           {/* main content */}
+           <div className="w-3/4 bg-green-800">
+           </div>
         </div>
     );
 }   
--- lib/src/db/schema.ts
+++ lib/src/db/schema.ts
@@ -6,5 +6,18 @@ export const usersTable = pgTable("users", {
     user_id: varchar({ length: 255 }).notNull().unique(),
     name: varchar({ length: 255 }).notNull(),
     email: varchar({ length: 255 }).notNull().unique(),
-    role: text({enum: ["employee", "verifier", "approver1", "approver2"]}).notNull()
   });
+
+const ROLES = ["employee", "verifier", "approver1", "approver2"] as const;
+const PERMISSIONS = ["create", "read", "update", "delete"] as const;
+
+export const user_roles = pgTable("user_roles", {
+    id: integer().primaryKey().generatedAlwaysAsIdentity(),
+    user_id: varchar({ length: 255 }).notNull().references(() => usersTable.user_id, { onDelete: "cascade" }),
+    role:text({enum: ROLES}).notNull()
+})
+export const rolePermissions = pgTable("role_permissions", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  role: text({ enum: ROLES }).notNull(),
+  permission: text({ enum: PERMISSIONS }).notNull()
+});
